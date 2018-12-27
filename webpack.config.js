f092b6dc0d70a07b5d88a93c4a9669cda4b8d337
Nathan Marley <nathan.marley@gmail.com>
2018-12-26 23:45:45 -0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    library: 'PaymentProtocol',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'dashcore-payment-protocol.js'
+  }
+};
