86498cc4b412efd81c24b81cbde794ee618c2fb7
Nathan Marley <nathan.marley@gmail.com>
2019-01-03 14:42:15 -0200

    Clean up files (#17)
    * Add a proper gitignore
    
    * Remove unused files from project
    
    Specifically:
    
    * project-level contributing
    * bower.json
    * gulpfile
    
    * Update rootcerts

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-// TODO: Remove previous line and work through linting issues at next edit.
-
-'use strict';
-
-var gulp_bitcore = require('bitcore-build-dash');
-
-gulp_bitcore('payment-protocol');
